483707c598340e90e141b62e4d34cfe76d518ce6
Martin A <mart.abberley@gmail.com>
2024-11-10 18:27:05 +0000

    Fixing import

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-deprecation-dates",
-    version="0.1.0",
+    version="0.1.1",
     description="A library to fetch Python version deprecation dates from endoflife.date.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
