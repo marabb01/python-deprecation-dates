eea75e05bd4bb0760cb50a15b0ebc2295db22575
Martin <mart.abberley@gmail.com>
2024-12-29 20:19:17 +0000

    Release/0.2.5 (#11)
    * Actually using load requirements function..
    
    * Setting package echosystem
    
    * Bumping version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def load_requirements(filename="requirements.txt"):
 
 setup(
     name="python-deprecation-dates",
-    version="0.2.4",
+    version="0.2.5",
     description="A library to fetch Python version deprecation dates from endoflife.date.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setup(
     author_email="mart.abberley@gmail.com",
     url="https://github.com/marabb01/python-deprecation-dates",
     packages=find_packages(),
-    install_requires=["requests==2.31.0"],
+    install_requires=[load_requirements()],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
