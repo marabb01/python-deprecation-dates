6efba308956f6f49d8d36b66a3625f12a3a0c80b
Martin A <mart.abberley@gmail.com>
2024-12-29 19:42:14 +0000

    Creating requirements file for package to be scanned by dependabot

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+
+def load_requirements(filename="requirements.txt"):
+    with open(filename, "r") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+
 setup(
     name="python-deprecation-dates",
     version="0.2.3",
@@ -10,7 +16,7 @@ setup(
     author_email="mart.abberley@gmail.com",
     url="https://github.com/marabb01/python-deprecation-dates",
     packages=find_packages(),
-    install_requires=["requests"],
+    install_requires=["requests==2.31.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
